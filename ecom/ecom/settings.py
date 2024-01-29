cd643025a6ccadae7f0a13d59b2050d1ab5a800a
Sajal K <sajalsaju687@gmail.com>
2024-01-29 19:09:56 +0530

    stngs

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY ="django-insecure-^-0sotv2&cl&gtl14(16574^rk9)+_fd$@!kq*%x#n4wdl&x-4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
