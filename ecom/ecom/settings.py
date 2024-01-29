a1e9461b8efea04130a4cb9e0884f4f9d447119e
Sajal K <147618636+Sajal-techie@users.noreply.github.com>
2024-01-29 21:10:47 +0530

    Update settings.py

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['13.49.65.117']]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
